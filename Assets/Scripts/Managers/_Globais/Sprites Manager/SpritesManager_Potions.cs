2d4f497775c88787107c69627bad8f0c6db39a5e
Mauricio Makimori <maumakimori@gmail.com>
2026-04-29 08:52:52 -0300

    Atualização do projeto
    Atualização do projeto

--- Assets/Scripts/Managers/_Globais/Sprites Manager/SpritesManager_Potions.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SpritesManager_Potions : MonoBehaviour
-{
-    public Sprite potion_explosion_icon;
-    public Sprite potion_restauration_icon;
-}
